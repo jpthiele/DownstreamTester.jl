a653fdf29e25d6fdfdd04fd9803c7a9226700916
Jan Philipp Thiele <thiele@wias-berlin.de>
2025-03-12 07:57:03 +0100

    Add deploydocs to documenter

--- docs/make.jl
+++ docs/make.jl
@@ -2,3 +2,7 @@ push!(LOAD_PATH, "../src")
 using Documenter, DownstreamTester, DocumenterMermaid
 
 makedocs(sitename = "DownstreamTester.jl")
+
+deploydocs(
+    repo= "github.com/jpthiele/DownstreamTester.jl.git"
+)
